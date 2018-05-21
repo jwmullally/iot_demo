d42089108ca2182f9f7ce29ee4fdcfb46b6a6b45
Joseph Mullally <jwmullally@gmail.com>
2018-05-21 11:44:35 +0100

    Add django-bootstrap4

--- iot_site/settings.py
+++ iot_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# bootstrap4 settings
+
+BOOTSTRAP4 = {
+    'javascript_in_head': True,
+    'include_jquery': 'slim',
+}
